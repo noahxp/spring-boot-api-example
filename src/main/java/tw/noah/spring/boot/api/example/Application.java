1a0b91d4929532c14310c3ac13fb072a5702f519
Noah Liu <noahxp@gmail.com>
2018-07-04 00:34:19 +0800

    build as a executable war file. war file

--- src/main/java/tw/noah/spring/boot/api/example/Application.java
+++ src/main/java/tw/noah/spring/boot/api/example/Application.java
@@ -2,11 +2,29 @@ package tw.noah.spring.boot.api.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//@SpringBootApplication
+//public class Application {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(Application.class, args);
+//	}
+//}
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+
 }
