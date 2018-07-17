7a9b9243637e282891833b0d7d722ae14575fffc
Noah Liu <noahxp@gmail.com>
2018-07-17 20:39:30 +0800

    日期格式轉型處理

--- src/main/java/tw/noah/spring/boot/api/example/control/CustomerController.java
+++ src/main/java/tw/noah/spring/boot/api/example/control/CustomerController.java
@@ -1,15 +1,21 @@
 package tw.noah.spring.boot.api.example.control;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import io.swagger.annotations.ResponseHeader;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import javax.annotation.Resource;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +24,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import tw.noah.spring.boot.api.example.entity.User;
@@ -80,6 +85,25 @@ public class CustomerController {
     ret.setMsg(JsonMsg.Success);
     return ret;
   }
+
+  @ApiOperation(value="生日計算年齡!",notes = "跟你說你你的生日，我就會回答你年齡")
+  @GetMapping(value="/birthday/{birthday}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public JsonModel sayBirthday(@ApiParam(value="your's birthday", example = "yyyy-MM-dd") @PathVariable(name = "birthday") @DateTimeFormat(pattern = "yyyy-MM-dd") Date birthday){
+
+    log.info(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(birthday));
+
+    int year = Period.between(birthday.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(), new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDate() ).getYears();
+    Map<String , Object> map = new HashMap<>();
+    map.put("age",year);
+    map.put("birthday",birthday);
+
+    JsonModel ret = new JsonModel();
+//    ret.setResult("your age is " + year);
+    ret.setResult(map);
+    ret.setMsg(JsonMsg.Success);
+    return ret;
+  }
+
 
   @ApiOperation(value="Ha一下",notes = "除了 Hi ，還有 Ha")
   @ApiResponses(value = {
--- src/main/java/tw/noah/spring/boot/api/example/entity/User.java
+++ src/main/java/tw/noah/spring/boot/api/example/entity/User.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.util.Date;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
 @ApiModel(value= "User Data" ,description = "使用者資料")
@@ -22,7 +23,7 @@ public class User {
   /**
    * user's birthday
    */
-  @ApiModelProperty(value="user birthday",required = false)
+  @ApiModelProperty(value="user birthday",required = false ,example = "yyyy-mm-dd")
   private Date birthday;
   /**
    * user's weight
