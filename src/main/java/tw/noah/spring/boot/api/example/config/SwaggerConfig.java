c720d624d9b8c76ddc1d44a1fd72a78ffd59807c
Noah Liu <noahxp@gmail.com>
2019-02-18 14:11:06 +0800

    add prodiction hidden swagger-ui

--- src/main/java/tw/noah/spring/boot/api/example/config/SwaggerConfig.java
+++ src/main/java/tw/noah/spring/boot/api/example/config/SwaggerConfig.java
@@ -2,9 +2,15 @@ package tw.noah.spring.boot.api.example.config;
 
 
 import com.google.common.base.Predicates;
+import javax.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -22,22 +28,25 @@ public class SwaggerConfig {
 
   @Bean
   public Docket productApi() {
-    ApiInfo info =  new ApiInfoBuilder()
-        .title("Spring Boot中使用Swagger2建置RESTful APIs")
-        .description("沒有Spring就沒有Java ^_^")
-        .termsOfServiceUrl("http://noah.tw")
-        .version("1.0")
-        .build();
-
+    ApiInfo info = new ApiInfoBuilder().title("Spring Boot中使用Swagger2建置RESTful APIs").description("沒有Spring就沒有Java ^_^")
+        .termsOfServiceUrl("http://noah.tw").version("1.0").build();
 
     // /error is the default not found exception
-    return new Docket(DocumentationType.SWAGGER_2)
-        .apiInfo(info)
-        .select()
-        .apis(RequestHandlerSelectors.basePackage(basePackage))
-        .paths(Predicates.not(PathSelectors.regex("/error")))
-        .build();
+    return new Docket(DocumentationType.SWAGGER_2).apiInfo(info).select().apis(RequestHandlerSelectors.basePackage(basePackage))
+        .paths(Predicates.not(PathSelectors.regex("/error"))).build();
 //            .paths(PathSelectors.any())
   }
 
+  /**
+   * production 透過 override 手法，將 swagger-ui 隱藏起來
+   */
+  @Profile("production")
+  @RestController
+  class DisableSwaggerUiController {
+
+    @RequestMapping(value = "/swagger-ui.html", method = RequestMethod.GET)
+    public void getSwagger(HttpServletResponse httpResponse) {
+      httpResponse.setStatus(HttpStatus.NOT_FOUND.value());
+    }
+  }
 }
